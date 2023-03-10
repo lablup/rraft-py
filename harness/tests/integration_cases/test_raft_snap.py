f414e8040bee38507d304c0432832eee3f6c1e6d
Gyubong <jopemachine@naver.com>
2023-03-10 16:31:28 +0900

    Add `test_sending_snapshot_set_pending_snapshot` test

--- harness/tests/integration_cases/test_raft_snap.py
+++ harness/tests/integration_cases/test_raft_snap.py
@@ -1,9 +1,56 @@
-def testing_snap():
-    pass
+import os
+import sys
+import warnings
+import pytest
+from typing import Dict, List
+
+from rraft import (
+    MessageType,
+    Snapshot_Owner,
+    default_logger,
+)
+
+from test_utils import (
+    new_snapshot,
+    new_storage,
+    new_test_raft,
+    new_message,
+    # Interface,
+)
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "../src"))
+sys.path.append(parent_dir)
+
+from interface import Interface
+from network import Network
+
+
+def testing_snap() -> Snapshot_Owner:
+    warnings.filterwarnings("ignore")
+    return new_snapshot(11, 11, [1, 2])
 
 
 def test_sending_snapshot_set_pending_snapshot():
-    pass
+    l = default_logger()
+    storage = new_storage()
+    sm = new_test_raft(1, [1, 2], 10, 1, storage.make_ref(), l.make_ref())
+    sm.raft.make_ref().restore(testing_snap())
+    sm.persist()
+
+    sm.raft.make_ref().become_candidate()
+    sm.raft.make_ref().become_leader()
+
+    # force set the next of node 1, so that
+    # node 1 needs a snapshot
+    assert sm.raft.make_ref().prs().get(2).get_next_idx() == sm.raft_log.first_index()
+
+    m = new_message(2, 1, MessageType.MsgAppendResponse, 0)
+    voter_2 = sm.raft.make_ref().prs().get(2)
+    m.make_ref().set_index(voter_2.get_next_idx() - 1)
+    m.make_ref().set_reject(True)
+
+    sm.step(m)
+    assert sm.raft.make_ref().prs().get(2).get_pending_snapshot() == 11
 
 
 def test_pending_snapshot_pause_replication():
