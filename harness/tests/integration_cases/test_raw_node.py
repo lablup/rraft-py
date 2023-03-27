8f3cec96f720a642dcee6f10e3a84d23b370df78
Gyubong <jopemachine@naver.com>
2023-03-27 18:35:36 +0900

    Add `test_raw_node_step`, `test_raw_node_read_index_to_old_leader` tests

--- harness/tests/integration_cases/test_raw_node.py
+++ harness/tests/integration_cases/test_raw_node.py
@@ -1,28 +1,218 @@
-def conf_change():
-    pass
+import os
+import sys
+import pytest
+from typing import Any, List, Optional, Tuple, cast
+from rraft import (
+    Config_Ref,
+    Entry_Owner,
+    Entry_Ref,
+    HardState_Ref,
+    Logger_Ref,
+    MemStorage_Owner,
+    MessageType,
+    RawNode__MemStorage_Owner,
+    Ready_Ref,
+    Snapshot_Owner,
+    Snapshot_Ref,
+    SoftState_Ref,
+    default_logger,
+    is_local_msg,
+)
+from test_utils import (
+    add_learner,
+    add_node,
+    conf_change_v2,
+    conf_change,
+    empty_entry,
+    new_entry,
+    new_message_with_entries,
+    new_message,
+    new_snapshot,
+    new_storage,
+    new_test_config,
+    new_test_raft_with_config,
+    new_test_raft_with_logs,
+    new_test_raft_with_prevote,
+    new_test_raft,
+    remove_node,
+    SOME_DATA,
+    hard_state,
+    # Interface,
+    # Network,
+)
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "../src"))
+sys.path.append(parent_dir)
+from interface import Interface
+from network import Network
 
 
-def must_cmp_ready():
-    pass
+def must_cmp_ready(
+    r: Ready_Ref,
+    ss: Optional[SoftState_Ref],
+    hs: Optional[HardState_Ref],
+    entries: List[Entry_Ref],
+    committed_entries: List[Entry_Ref],
+    snapshot: Optional[Snapshot_Ref],
+    msg_is_empty: bool,
+    persisted_msg_is_empty: bool,
+    must_sync: bool,
+):
+    assert r.ss() == ss
+    assert r.hs() == hs
+    assert r.entries() == entries
+    assert r.committed_entries() == committed_entries
+    assert r.must_sync() == must_sync
+    assert r.snapshot() == snapshot or r.snapshot() == Snapshot_Owner.default()
+    assert len(r.msgs()) == 0 if msg_is_empty else len(r.msgs()) != 0
+    assert (
+        len(r.persisted_msgs()) == 0
+        if persisted_msg_is_empty
+        else len(r.persisted_msgs()) != 0
+    )
 
 
-def new_raw_node():
-    pass
+def new_raw_node(
+    id: int,
+    peers: List[int],
+    election_tick: int,
+    heartbeat_tick: int,
+    storage: MemStorage_Owner,
+    logger: Logger_Ref,
+) -> RawNode__MemStorage_Owner:
+    config = new_test_config(id, election_tick, heartbeat_tick)
+    return new_raw_node_with_config(peers, config, storage, logger)
 
 
-def new_raw_node_with_config():
-    pass
+def new_raw_node_with_config(
+    peers: List[int],
+    config: Config_Ref,
+    storage: MemStorage_Owner,
+    logger: Logger_Ref,
+) -> RawNode__MemStorage_Owner:
+    assert not (
+        storage.initial_state().initialized() and not peers
+    ), f"new_raw_node with empty peers on initialized store"
+
+    if peers and not storage.initial_state().initialized():
+        storage.wl(lambda core: core.apply_snapshot(new_snapshot(1, 1, peers)))
+
+    return RawNode__MemStorage_Owner(config, storage, logger)
+
+
+def get_msg_types() -> List[MessageType]:
+    return [
+        MessageType.MsgHup,
+        MessageType.MsgBeat,
+        MessageType.MsgPropose,
+        MessageType.MsgAppend,
+        MessageType.MsgAppendResponse,
+        MessageType.MsgRequestVote,
+        MessageType.MsgRequestVoteResponse,
+        MessageType.MsgSnapshot,
+        MessageType.MsgHeartbeat,
+        MessageType.MsgHeartbeatResponse,
+        MessageType.MsgUnreachable,
+        MessageType.MsgSnapStatus,
+        MessageType.MsgCheckQuorum,
+        MessageType.MsgTransferLeader,
+        MessageType.MsgTimeoutNow,
+        MessageType.MsgReadIndex,
+        MessageType.MsgReadIndexResp,
+        MessageType.MsgRequestPreVote,
+        MessageType.MsgRequestPreVoteResponse,
+    ]
 
 
 # Ensures that RawNode::step ignore local message.
 def test_raw_node_step():
-    pass
+    l = default_logger()
+    for msg_t in get_msg_types():
+        s = new_storage()
+        s.wl(lambda core: core.set_hardstate(hard_state(1, 1, 0)))
+        # Append an empty entry to make sure the non-local messages (like
+        # vote requests) are ignored and don't trigger assertions.
+        s.wl(lambda core: core.append([new_entry(1, 1, None)]))
+        s.wl(lambda core: core.apply_snapshot(new_snapshot(1, 1, [1])))
+
+        storage = new_storage()
+        raw_node = new_raw_node(1, [1], 10, 1, storage, l)
+        # LocalMsg should be ignored.
+        try:
+            raw_node.step(new_message(0, 0, msg_t, 0))
+        except Exception as e:
+            if is_local_msg(msg_t):
+                assert str(e) == "raft: cannot step raft local message"
 
 
 # Ensures that MsgReadIndex to old leader gets forwarded to the new leader and
 # 'send' method does not attach its term.
 def test_raw_node_read_index_to_old_leader():
-    pass
+    l = default_logger()
+    s1, s2, s3 = new_storage(), new_storage(), new_storage()
+    r1 = new_test_raft(1, [1, 2, 3], 10, 1, s1, l)
+    r2 = new_test_raft(2, [1, 2, 3], 10, 1, s2, l)
+    r3 = new_test_raft(3, [1, 2, 3], 10, 1, s3, l)
+
+    nt = Network.new([r1, r2, r3], l)
+
+    # elect r1 as leader
+    nt.send([new_message(1, 1, MessageType.MsgHup, 0)])
+
+    test_entries = Entry_Owner.default()
+    test_entries.set_data(b"testdata")
+
+    # send readindex request to r2(follower)
+    _ = nt.peers.get(2).step(
+        new_message_with_entries(
+            2,
+            2,
+            MessageType.MsgReadIndex,
+            [test_entries.clone()],
+        )
+    )
+
+    # verify r2(follower) forwards this message to r1(leader) with term not set
+    assert len(nt.peers[2].raft.get_msgs()) == 1
+    read_index_msg1 = new_message_with_entries(
+        2, 1, MessageType.MsgReadIndex, [test_entries.clone()]
+    )
+    assert read_index_msg1 == nt.peers[2].raft.get_msgs()[0]
+
+    # send readindex request to r3(follower)
+    _ = nt.peers.get(3).step(
+        new_message_with_entries(
+            3,
+            3,
+            MessageType.MsgReadIndex,
+            [test_entries.clone()],
+        )
+    )
+
+    # verify r3(follower) forwards this message to r1(leader) with term not set as well.
+    assert len(nt.peers[3].raft.get_msgs()) == 1
+
+    read_index_msg2 = new_message_with_entries(
+        3, 1, MessageType.MsgReadIndex, [test_entries.clone()]
+    )
+    assert nt.peers[3].raft.get_msgs()[0] == read_index_msg2
+
+    # now elect r3 as leader
+    nt.send([new_message(3, 3, MessageType.MsgHup, 0)])
+
+    # let r1 steps the two messages previously we got from r2, r3
+    _ = nt.peers.get(1).step(read_index_msg1)
+    _ = nt.peers.get(1).step(read_index_msg2)
+
+    # verify r1(follower) forwards these messages again to r3(new leader)
+    assert len(nt.peers[1].raft.get_msgs()) == 2
+    assert nt.peers[1].raft.get_msgs()[0] == new_message_with_entries(
+        2, 3, MessageType.MsgReadIndex, [test_entries.clone()]
+    )
+
+    assert nt.peers[1].raft.get_msgs()[1] == new_message_with_entries(
+        3, 3, MessageType.MsgReadIndex, [test_entries]
+    )
 
 
 # Tests the configuration change mechanism. Each test case sends a configuration
--- src/prelude/raft.rs
+++ src/prelude/raft.rs
@@ -249,8 +249,7 @@ impl Py_Raft__MemStorage_Ref {
     }
 
     pub fn load_state(&mut self, hs: Py_HardState_Mut) -> PyResult<()> {
-        self.inner
-            .map_as_mut(|inner| inner.load_state(&hs.into()))
+        self.inner.map_as_mut(|inner| inner.load_state(&hs.into()))
     }
 
     pub fn soft_state(&self) -> PyResult<Py_SoftState_Ref> {
