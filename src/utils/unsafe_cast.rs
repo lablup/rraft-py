36ad3d8da27737946018dd6f442f74667731f985
Gyubong <jopemachine@naver.com>
2023-11-12 20:34:54 +0900

    Add `invalid_reference_casting` allow

--- src/utils/unsafe_cast.rs
+++ src/utils/unsafe_cast.rs
@@ -13,6 +13,7 @@ pub unsafe fn make_static<T: ?Sized>(ref_: &T) -> &'static T {
 /// TODO: Write some warnings here.
 #[inline]
 pub unsafe fn make_mut<T: ?Sized>(ref_: &T) -> &mut T {
+    #![allow(invalid_reference_casting)]
     #![allow(clippy::mut_from_ref)]
     &mut *(ref_ as *const T as *mut T)
 }
